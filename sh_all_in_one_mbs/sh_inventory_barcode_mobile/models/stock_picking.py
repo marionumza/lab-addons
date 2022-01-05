b41b41dea8f4ab5224c0a6f4bd4467f2898b42ea
JorgeQuinteros <quinterosorellana04@gmail.com>
2022-01-05 18:11:45 +0100

    [IMP] Mark package done in assigned state

--- sh_all_in_one_mbs/sh_inventory_barcode_mobile/models/stock_picking.py
+++ sh_all_in_one_mbs/sh_inventory_barcode_mobile/models/stock_picking.py
@@ -52,108 +52,6 @@ class StockPicking(models.Model):
                     (self._cr.dbname, 'res.partner', self.env.user.partner_id.id),
                     {'type': 'simple_notification', 'title': _('Failed'), 'message': message, 'sticky': False, 'warning': True})
             return
-
-#         elif self and self.picking_type_code in ['outgoing','internal']:
-#             search_mls = False
-#             domain = []
-#
-#             if self.env.user.company_id.sudo().sh_stock_barcode_mobile_type == 'barcode':
-#                 search_mls = self.move_line_ids_without_package.filtered(lambda ml: ml.product_id.barcode == self.sh_stock_barcode_mobile)
-#                 domain = [("barcode","=",self.sh_stock_barcode_mobile)]
-#
-#             elif self.env.user.company_id.sudo().sh_stock_barcode_mobile_type == 'int_ref':
-#                 search_mls = self.move_line_ids_without_package.filtered(lambda ml: ml.product_id.default_code == self.sh_stock_barcode_mobile)
-#                 domain = [("default_code","=",self.sh_stock_barcode_mobile)]
-#
-#             elif self.env.user.company_id.sudo().sh_stock_barcode_mobile_type == 'sh_qr_code':
-#                 search_mls = self.move_line_ids_without_package.filtered(lambda ml: ml.product_id.sh_qr_code == self.sh_stock_barcode_mobile)
-#                 domain = [("sh_qr_code","=",self.sh_stock_barcode_mobile)]
-#
-#             elif self.env.user.company_id.sudo().sh_stock_barcode_mobile_type == 'all':
-#                 search_mls = self.move_line_ids_without_package.filtered(lambda ml: ml.product_id.barcode == self.sh_stock_barcode_mobile
-#                                                                          or ml.product_id.default_code == self.sh_stock_barcode_mobile
-#                                                                          or ml.product_id.sh_qr_code == self.sh_stock_barcode_mobile
-#                                                                          )
-#                 domain = ["|","|",
-#                     ("default_code","=",self.sh_stock_barcode_mobile),
-#                     ("barcode","=",self.sh_stock_barcode_mobile),
-#                     ("sh_qr_code","=",self.sh_stock_barcode_mobile)
-#                 ]
-#
-#             if search_mls:
-#                 for move_line in search_mls:
-#                     if move_line.show_details_visible:
-#                         if self.env.user.company_id.sudo().sh_stock_bm_is_notify_on_fail:
-#                             message = _(CODE_SOUND_FAIL + 'You can not scan product item for Detailed Operations directly here, Pls click detail button (at end each line) and than rescan your product item.')
-#                             self.env['bus.bus'].sendone(
-#                                 (self._cr.dbname, 'res.partner', self.env.user.partner_id.id),
-#                                 {'type': 'simple_notification', 'title': _('Failed'), 'message': message, 'sticky': False, 'warning': True})
-#
-#                         return
-#
-#                     move_line.qty_done += 1
-#                     if self.env.user.company_id.sudo().sh_stock_bm_is_notify_on_success:
-#                         message = _(CODE_SOUND_SUCCESS + 'Product: %s Qty: %s') % (move_line.product_id.name,move_line.qty_done)
-#                         self.env['bus.bus'].sendone(
-#                             (self._cr.dbname, 'res.partner', self.env.user.partner_id.id),
-#                             {'type': 'simple_notification', 'title': _('Succeed'), 'message': message, 'sticky': False, 'warning': False})
-#                     break
-#
-#             elif self.state == 'draft':
-#                 if self.env.user.company_id.sudo().sh_stock_bm_is_add_product:
-#                     search_product = self.env["product.product"].search(domain, limit = 1)
-#                     if search_product:
-#                         stock_move_line_vals = {
-#                            "product_id": search_product.id,
-#                            "qty_done" : 1,
-#                            "location_id" : self.location_id.id,
-#                            "location_dest_id": self.location_dest_id.id,
-#                            'date' : self.scheduled_date or fields.date.today(),
-#                            'company_id' : self.company_id.id if self.company_id else self.env.user.company_id.id
-#                         }
-#                         if search_product.uom_id:
-#                             stock_move_line_vals.update({
-#                                 "product_uom_id": search_product.uom_id.id,
-#                             })
-#
-#                         old_lines = self.move_line_ids_without_package
-#                         new_order_line = self.move_line_ids_without_package.create(stock_move_line_vals)
-#                         self.move_line_ids_without_package = old_lines + new_order_line
-#                         new_order_line.onchange_product_id()
-#
-#                         if self.env.user.company_id.sudo().sh_stock_bm_is_notify_on_success:
-#                             message = _(CODE_SOUND_SUCCESS + 'Product: %s Qty: %s') % (new_order_line.product_id.name,new_order_line.product_uom_qty)
-#                             self.env['bus.bus'].sendone(
-#                                 (self._cr.dbname, 'res.partner', self.env.user.partner_id.id),
-#                                 {'type': 'simple_notification', 'title': _('Succeed'), 'message': message, 'sticky': False, 'warning': False})
-#                         return
-#                     else:
-#                         if self.env.user.company_id.sudo().sh_stock_bm_is_notify_on_fail:
-#                             message = _(CODE_SOUND_FAIL + 'Scanned Internal Reference/Barcode not exist in any product!')
-#                             self.env['bus.bus'].sendone(
-#                                 (self._cr.dbname, 'res.partner', self.env.user.partner_id.id),
-#                                 {'type': 'simple_notification', 'title': _('Failed'), 'message': message, 'sticky': False, 'warning': True})
-#                         return
-#
-#
-#                 else:
-#                     if self.env.user.company_id.sudo().sh_stock_bm_is_notify_on_fail:
-#                         message = _(CODE_SOUND_FAIL + 'Scanned Internal Reference/Barcode not exist in any product!')
-#                         self.env['bus.bus'].sendone(
-#                             (self._cr.dbname, 'res.partner', self.env.user.partner_id.id),
-#                             {'type': 'simple_notification', 'title': _('Failed'), 'message': message, 'sticky': False, 'warning': True})
-#
-#                     return
-#             else:
-#                 if self.env.user.company_id.sudo().sh_stock_bm_is_notify_on_fail:
-#                     message = _(CODE_SOUND_FAIL + 'Scanned Internal Reference/Barcode not exist in any product!')
-#                     self.env['bus.bus'].sendone(
-#                         (self._cr.dbname, 'res.partner', self.env.user.partner_id.id),
-#                         {'type': 'simple_notification', 'title': _('Failed'), 'message': message, 'sticky': False, 'warning': True})
-#
-#                 return
-#
-#
         elif self:
             search_mls = False
             domain = []
@@ -194,7 +92,6 @@ class StockPicking(models.Model):
                                 {'type': 'simple_notification', 'title': _('Failed'), 'message': message, 'sticky': False, 'warning': True})
 
                         return
-
                     move_line.quantity_done += 1
                     if self.env.user.company_id.sudo().sh_stock_bm_is_notify_on_success:
                         message = _(CODE_SOUND_SUCCESS + 'Product: %s Qty: %s') % (
@@ -207,9 +104,11 @@ class StockPicking(models.Model):
                     break
             elif self.state == 'draft':
                 if self.env.user.company_id.sudo().sh_stock_bm_is_add_product:
-
                     search_product = self.env["product.product"].search(
                         domain, limit=1)
+                    search_package = self.env['stock.package_level'].search(
+                        domain, limit=1
+                    )
                     if search_product:
                         stock_move_vals = {
                             "name": search_product.name,
@@ -237,18 +136,16 @@ class StockPicking(models.Model):
                                  self.env.user.partner_id.id),
                                 {'type': 'simple_notification', 'title': _('Succeed'), 'message': message, 'sticky': False, 'warning': False})
                         return
-
                     else:
                         if self.env.user.company_id.sudo().sh_stock_bm_is_notify_on_fail:
                             message = _(
-                                CODE_SOUND_FAIL + 'Scanned Internal Reference/Barcode not exist in any product!')
+                                CODE_SOUND_FAIL + 'Scanned Internal Reference/Barcode not exist in any product/package!')
                             self.env['bus.bus'].sendone(
                                 (self._cr.dbname, 'res.partner',
                                  self.env.user.partner_id.id),
                                 {'type': 'simple_notification', 'title': _('Failed'), 'message': message, 'sticky': False, 'warning': True})
 
                         return
-
                 else:
                     if self.env.user.company_id.sudo().sh_stock_bm_is_notify_on_fail:
                         message = _(
@@ -259,7 +156,24 @@ class StockPicking(models.Model):
                             {'type': 'simple_notification', 'title': _('Failed'), 'message': message, 'sticky': False, 'warning': True})
 
                     return
-
+            elif self.state == 'assigned':
+                search_package = self.env['stock.package_level'].search(
+                    [('package_id', '=', self.sh_stock_barcode_mobile)],
+                    limit=1,
+                )
+                if search_package:
+                    for line in self.package_level_ids_details:
+                        if self.sh_stock_barcode_mobile == line.package_id.name:
+                            line.is_done = True
+                else:
+                    if self.env.user.company_id.sudo().sh_stock_bm_is_notify_on_fail:
+                        message = _(
+                            CODE_SOUND_FAIL + 'Scanned Internal Reference/Barcode not exist in any product/package!')
+                        self.env['bus.bus'].sendone(
+                            (self._cr.dbname, 'res.partner',
+                                self.env.user.partner_id.id),
+                            {'type': 'simple_notification', 'title': _('Failed'), 'message': message, 'sticky': False, 'warning': True}) 
+                    return
             else:
                 if self.env.user.company_id.sudo().sh_stock_bm_is_notify_on_fail:
                     message = _(
@@ -280,3 +194,7 @@ class StockPicking(models.Model):
                     {'type': 'simple_notification', 'title': _('Failed'), 'message': message, 'sticky': False, 'warning': True})
 
             return
+
+    def write(self, vals):
+        res = super().write(vals)
+        return res
